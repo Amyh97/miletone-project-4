c42b2418656baa12956988857a8ecdb7376ee2fd
Amyh97 <amy.hollis@btinternet.com>
2021-02-03 11:37:19 +0000

    add home app, templates and static file

--- amy_hollis_photography/settings.py
+++ amy_hollis_photography/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-
     # project apps
     'home'
 ]
@@ -63,7 +62,10 @@ ROOT_URLCONF = 'amy_hollis_photography.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
