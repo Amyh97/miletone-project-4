d97042eb4fc2ef9711ec1dcb346a845b960e300b
Amyh97 <amy.hollis@btinternet.com>
2021-02-12 16:50:28 +0000

    add products detail page with size and finish options

--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .models import products
+from .models import products, sizes, finish
 
 
 # Create your views here.
@@ -22,3 +22,18 @@ def products_page(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to display the detail of a selected image """
+    # pk is first field in json file for products data
+    product = get_object_or_404(products, pk=product_id)
+    size = sizes.objects.all()
+    finishes = finish.objects.all()
+    context = {
+        'product': product,
+        'sizes': size,
+        'finishes': finishes,
+    }
+
+    return render(request, 'products/product_detail.html', context)
