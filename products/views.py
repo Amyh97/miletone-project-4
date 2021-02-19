6ef49fd2fe69dafab6191b4be28b35d63e25e527
Amyh97 <amy.hollis@btinternet.com>
2021-02-19 11:36:29 +0000

    add filter functionality

--- products/views.py
+++ products/views.py
@@ -7,31 +7,35 @@ from .models import products, sizes, finish, categories
 def products_page(request):
     """ A view to display all images for sale"""
     category = categories.objects.all()
-    product = products.objects.all()
-    #category_sort = None
-    search_product = None
+    product = None
 
-    #if request.GET:
-    #if 'category_sort' in request.GET:
-    #category = request.GET['category_sort']
-    # double underscore is common Django syntax for queries
-    search_product = product.filter(category__name__in=category)
-    category_sort = category.filter(name__in=category)
+    if request.GET:
+        if 'CategorySort' in request.GET:
+            CategorySort = request.GET['CategorySort']
+            # double underscore is common Django syntax for queries
+            product = products.objects.filter(category__name=CategorySort)
 
-    page = request.GET.get('page', 1)
+    else:
+        product = products.objects.all()
 
-    paginator = Paginator(product, 10)
-    try:
-        product = paginator.page(page)
-    except PageNotAnInteger:
-        product = paginator.page(1)
-    except EmptyPage:
-        product = paginator.page(paginator.num_pages)
+    if product.__len__ >= 11:
+        hide = False
+        page = request.GET.get('page', 1)
+
+        paginator = Paginator(product, 10)
+        try:
+            product = paginator.page(page)
+        except PageNotAnInteger:
+            product = paginator.page(1)
+        except EmptyPage:
+            product = paginator.page(paginator.num_pages)
+    else:
+        hide = True
+
     context = {
         'products': product,
         'categories': category,
-        'current_categories': category_sort,
-        'search_product': search_product,
+
     }
 
     return render(request, 'products/products.html', context)
