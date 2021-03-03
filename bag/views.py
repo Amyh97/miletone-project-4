21b690c48e3f52c7bede4bc00b54cc386c536c36
Amyh97 <amy.hollis@btinternet.com>
2021-03-03 23:22:08 +0000

    use JS to fill in form to add items to session basket and add basket functionailty

--- bag/views.py
+++ bag/views.py
@@ -7,24 +7,30 @@ def bag(request):
 
 
 def add_to_basket(request, item_id):
-    """ add items, product or services to the basket """
+    if request.POST:
+        name = request.POST.get('name')
+        quantity = int(request.POST.get('quantity'))
+        price = request.POST.get('price')
+        size_len = request.POST.get('size_len')
+        finish_img = request.POST.get('finish_img')
+        image = request.POST.get('image')
+        redirect_url = request.POST.get('redirect_url')
+        basket = request.session.get('basket', {})
+        basket_item = {
+            'name': name,
+            'quantity': quantity,
+            'price': price,
+            'size_len': size_len,
+            'finish_img': finish_img,
+            'image': image,
+        }
+        if item_id in list(basket.keys()):
+            basket_item = str(basket_item)
+            basket[basket_item] += quantity
+        else:
+            basket_item = str(basket_item)
+            basket[basket_item] = quantity
 
-    quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
-    # session to store this particular basket
-    # get basket or initialise new one if not found
-    basket = request.session.get('basket', {})
+        request.session['basket'] = basket
 
-    if item_id in list(basket.keys()):
-        # if item is already in basket, add x amount
-        basket[item_id] += quantity
-    else:
-        # if not already in the basket, add it
-        basket[item_id] = quantity
-
-    # update session basket
-    request.session['basket'] = basket
-
-    print(request.session['basket'])
-    # bring users back to where they were
-    return(redirect(redirect_url))
+        return redirect(redirect_url)
