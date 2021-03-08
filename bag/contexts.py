0feac06a7737b52a17a23169d1b40ac25963b22d
Amyh97 <amy.hollis@btinternet.com>
2021-03-08 15:43:43 +0000

    correct how items are added to cart and add ability to add more to basket

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,30 +1,11 @@
 from decimal import Decimal
 from django.conf import settings
-from .views import basket_item
 
 
 def basket_content(request):
     basket_items = []
     total = 0
     count = 0
-    basket = request.session.get('basket', {})
-    item = basket_item
-
-    for item in basket.items():
-        # item is tuple, element 0 is string version of dict
-        split_items = item[0].split(",")
-        # get the value after ":" of quantity and price items
-        quantity = int(split_items[1].split(":")[1])
-        price = split_items[2].split(":")[1]
-        # remove excess quote marks from sting
-        price_tidy = price.replace("'", "")
-        # convert to decimal to do maths on
-        float_price = Decimal(price_tidy)
-        total += quantity * float_price
-        count += quantity
-        basket_items.append({
-                'item': item
-            })
 
     if total < settings.FREE_DELIVERY:
         # decimal used rather than float as float is more susceptible to errors
--- bag/views.py
+++ bag/views.py
@@ -3,11 +3,13 @@ from django.shortcuts import render, redirect
 
 def bag(request):
     """ a view to return bag content """
+
     return render(request, 'bag/bag.html')
 
 
 def basket_item(request, item_id):
     if request.POST:
+        item_id = request.POST.get('id')
         name = request.POST.get('name')
         quantity = int(request.POST.get('quantity'))
         price = request.POST.get('price')
@@ -16,22 +18,17 @@ def basket_item(request, item_id):
         image = request.POST.get('image')
         basket = request.session.get('basket', {})
         redirect_url = request.POST.get('redirect_url')
-        item = {
-            'name': name,
-            'quantity': quantity,
-            'price': price,
-            'size_len': size_len,
-            'finish_img': finish_img,
-            'image': image,
-        }
+        to_add = (item_id, name, size_len, finish_img, price, image)
+        item_in_basket = "-".join(to_add)
 
-    if item in list(basket.keys()):
-        item = str(item)
-        basket[item] += quantity
-
+    if item_id in list(basket.keys()):
+        if item_in_basket in basket[item_id]['items_by_specs'].keys():
+            # add variable accesses the last item in the item_in_basket dict
+            add = int(basket[item_id]['items_by_specs'][item_in_basket])
+            add += quantity
+            basket[item_id]['items_by_specs'][item_in_basket] += add
     else:
-        item = str(item)
-        basket[item] = quantity
+        basket[item_id] = {'items_by_specs': {item_in_basket: quantity}}
 
     request.session['basket'] = basket
     return redirect(redirect_url)
