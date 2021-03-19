e1a0e91a95cba1bb0b9b6accc0e247297bb490cb
Amyh97 <amy.hollis@btinternet.com>
2021-03-19 15:18:32 +0000

    secure views with requires login and check for superuser

--- products/views.py
+++ products/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from .models import products, sizes, finish, categories
 from .forms import ProductForm
 
 
-# Create your views here.
 def products_page(request):
     """ A view to display all images for sale"""
     category = categories.objects.all()
@@ -54,61 +54,80 @@ def product_detail(request, product_id):
     return render(request, 'products/product_detail.html', context)
 
 
+@login_required
 def add_product(request):
     """ add products to site """
-
-    if request.method == 'POST':
-        form = ProductForm(request.POST, request.FILES)
-        if form.is_valid():
-            product = form.save()
-            messages.success(request, 'Product has successfully been added\
-                to the website.')
-            return redirect(reverse('product_detail', args=[product.id]))
-        else:
-            messages.error(request, 'There was in error in the form, please\
-                double check the data and try again.')
+    if not request.user.is_superuser():
+        messages.error(request, 'Sorry! Only site admin can add\
+            products to the site')
+        return redirect(reverse('home'))
     else:
-        form = ProductForm()
+        if request.method == 'POST':
+            form = ProductForm(request.POST, request.FILES)
+            if form.is_valid():
+                product = form.save()
+                messages.success(request, 'Product has successfully been added\
+                    to the website.')
+                return redirect(reverse('product_detail', args=[product.id]))
+            else:
+                messages.error(request, 'There was in error in the form, please\
+                    double check the data and try again.')
+        else:
+            form = ProductForm()
 
-    template = 'products/add_to_store.html'
-    context = {
-        'form': form,
-    }
+        template = 'products/add_to_store.html'
+        context = {
+            'form': form,
+        }
 
-    return render(request, template, context)
+        return render(request, template, context)
 
 
+@login_required
 def edit_product(request, product_id):
     """ edit products on the site """
-    product = get_object_or_404(products, pk=product_id)
+    if not request.user.is_superuser():
+        messages.error(request, 'Sorry! Only site admin can edit\
+            products on the site')
+        return redirect(reverse('home'))
 
-    if request.method == 'POST':
-        form = ProductForm(request.POST, request.FILES, instance=product)
-        if form.is_valid():
-            form.save()
-            messages.success(request, f'You have successfully\
-                updated {product.name}!')
-            return redirect(reverse('product_detail', args=[product.id]))
-        else:
-            messages.error(request, f'Could not update {product.name}.\
-                Please double check the form and try again.')
     else:
-        form = ProductForm(instance=product)
-        messages.info(request, f'You are editing {product.name}')
 
-    template = 'products/edit_item.html'
+        product = get_object_or_404(products, pk=product_id)
 
-    context = {
-        'form': form,
-        'product': product,
-    }
+        if request.method == 'POST':
+            form = ProductForm(request.POST, request.FILES, instance=product)
+            if form.is_valid():
+                form.save()
+                messages.success(request, f'You have successfully\
+                    updated {product.name}!')
+                return redirect(reverse('product_detail', args=[product.id]))
+            else:
+                messages.error(request, f'Could not update {product.name}.\
+                    Please double check the form and try again.')
+        else:
+            form = ProductForm(instance=product)
+            messages.info(request, f'You are editing {product.name}')
 
-    return render(request, template, context)
+        template = 'products/edit_item.html'
+
+        context = {
+            'form': form,
+            'product': product,
+        }
+
+        return render(request, template, context)
 
 
+@login_required
 def delete_product(request, product_id):
-    product = get_object_or_404(products, pk=product_id)
-    product.delete()
-    messages.warning(request, f'{product.name} has been removed\
-        from the website')
-    return redirect(reverse('products'))
+    if not request.user.is_superuser():
+        messages.error(request, 'Sorry! Only site admin can delete\
+            products from the site')
+        return redirect(reverse('home'))
+    else:
+        product = get_object_or_404(products, pk=product_id)
+        product.delete()
+        messages.warning(request, f'{product.name} has been removed\
+            from the website')
+        return redirect(reverse('products'))
--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import ProfileForm
 from .models import UserProfile
 from checkout.models import Order
 
 
+@login_required
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
@@ -33,6 +35,7 @@ def profile(request):
     return render(request, template, context)
 
 
+@login_required
 def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
 
