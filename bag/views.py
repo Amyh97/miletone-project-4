2b30b4a66a7515673a71f5ddcf27f8656ac8a9e8
Amyh97 <amy.hollis@btinternet.com>
2021-03-23 16:56:05 +0000

    restructure basket to make code less complex and reusable

--- bag/views.py
+++ bag/views.py
@@ -14,6 +14,7 @@ def bag(request):
 def basket_item(request, item_id):
     """ a view to add items to a bag based on specs """
     if request.POST:
+        item_type = request.POST.get('type')
         item_id = request.POST.get('id')
         name = request.POST.get('name')
         quantity = int(request.POST.get('quantity'))
@@ -23,33 +24,45 @@ def basket_item(request, item_id):
         image = request.POST.get('image')
         basket = request.session.get('basket', {})
         redirect_url = request.POST.get('redirect_url')
-        item_in_basket = {}
-        to_add = (item_id, name, size_len, finish_img, price, image)
-        item_in_basket = "_".join(to_add)
 
-    if item_id in list(basket.keys()):
-        if item_in_basket in basket[item_id]['items_by_specs'].keys():
-            # add variable accesses the last item in the item_in_basket dict
-            add = int(basket[item_id]['items_by_specs'][item_in_basket])
-            quantity = add + quantity
-            basket[item_id]['items_by_specs'][item_in_basket] += quantity - add
-            messages.success(request, f'Added {quantity} {name}, {size_len},\
-                        {finish_img} to your basket.')
+        keys = ('name', 'image', 'size_len', 'finish_img', 'price', 'quantity')
+        values = (name, image, size_len, finish_img, price, quantity)
 
+        if item_type == 'product':
+            basket_products = request.session.get('basket_products', {})
+            specs = '[{}]'.format(', '.join("'{}': '{}'".format(k, v) for k, v in zip(keys, values)))
+            # list as dict is not hashable
+            if item_id not in list(basket_products):
+                basket_products[item_id] = specs
+                messages.success(request, f'Added {quantity} {name}, {size_len},\
+                            {finish_img} to your basket.')
+                request.session['basket_products'] = basket_products
+            else:
+                this_size_len = basket_products[item_id].split(',')[2].split(':')[1].replace("'", "").strip()
+                this_finish_img = basket_products[item_id].split(',')[3].split(':')[1].replace("'", "").strip()
+                if this_size_len == size_len and this_finish_img == finish_img:
+                    # what is alreay in the basket
+                    current_qty = int(basket_products[item_id].split(',')[5].split(':')[1].replace("']", "").replace("'", "").strip())
+                    # ensure the number is increased, not overwritten
+                    new_qty = str(current_qty + quantity)
+                    # push new quantity into specs
+                    new_specs = specs.replace(f"'{current_qty}']'", new_qty)
+                    basket_products[item_id] = new_specs
+                    messages.success(request, f'Added another {quantity} to {name}, {size_len},\
+                            {finish_img} to your basket.')
+                else:
+                    # create a more unique id to prevent overwriting other id
+                    item_id = item_id+size_len+finish_img
+                    specs = '[{}]'.format(', '.join("'{}': '{}'".format(k, v) for k, v in zip(keys, values)))
+                    basket_products[item_id] = specs
+                    request.session['basket_products'] = basket_products
+
+                    messages.success(request, f'Added {quantity} {name}, {size_len},\
+                                {finish_img} to your basket.')
+            basket = basket_products
         else:
-            """
-            assign a more unique id if image is already in basket
-            with different specs
-            """
-            item_id = item_id + size_len + finish_img
-            basket[item_id] = {'items_by_specs': {item_in_basket: quantity}}
-            messages.success(request, f'Added {quantity} {name}, {size_len},\
-                        {finish_img} to your basket.')
+            print('services will be added later')
 
-    else:
-        basket[item_id] = {'items_by_specs': {item_in_basket: quantity}}
-        messages.success(request, f'Added {quantity} {name}, {size_len},\
-                        {finish_img} to your basket.')
 
     request.session['basket'] = basket
 
@@ -57,48 +70,8 @@ def basket_item(request, item_id):
 
 
 def adjust_basket(request, item_id):
-    """Update quantity of a specific product in the shopping cart"""
-    quantity = int(request.POST.get('quantity'))
-    basket = request.session.get('basket', {})
-    product = get_object_or_404(products, pk=item_id)
-
-    if quantity > 0:
-        # use if so code can be run on each item in basket
-        if item_id in basket:
-            # each item in basket is 3 dictionaries nested in eachother
-            # turn values into list, dictionary containing specs and quantity
-            basket_item_id = list(basket[item_id].values())
-            # turn item into string so it can be split and manipulated
-            dict_string = str(basket_item_id[0])
-            # remove quotes from string to tidy for context processor
-            remove_quotes = dict_string.replace("'", "")
-            # split string at : to get value of key value pair
-            split_val = remove_quotes.split(":")[0]
-            """ remove remaining curly brace so string matches
-            the original val of items_by_specs """
-            change_quantity = split_val.replace("{", "")
-            basket[item_id]['items_by_specs'][change_quantity] = quantity
-        messages.success(request, f'The quantity of {product.name}\
-                    has been changed to {quantity}.')
-    else:
-        del basket[item_id]
-        messages.warning(request, f'{product.name} has been\
-                        removed from your basket')
-
-    request.session['basket'] = basket
-    return redirect(reverse('bag'))
+    print('test')
 
 
 def remove_from_basket(request, item_id):
-    """ Remove a specific product in the shopping cart """
-    product = get_object_or_404(products, pk=item_id)
-    try:
-        basket = request.session.get('basket', {})
-        basket.pop(item_id)
-        request.session['basket'] = basket
-        messages.warning(request, f'{product.name} has been\
-                        removed from your basket')
-        return HttpResponse(status=200)
-    except Exception as e:
-        messages.error(request, f'Error removing item: {e}')
-        return HttpResponse(status=500)
+    print('test')
